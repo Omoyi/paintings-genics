8ac8b3454df38ae7377afce021efd4d1a1ebba43
Manirakora Alexie <52699712+Belie06Loryn@users.noreply.github.com>
2019-08-09 21:12:45 +0200

    Add files via upload

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,22 @@
-
+// Business logic
+$(document).ready(function(){
+  var modal = document.getElementById('id01');
+  window.onclick = function(event) {
+    event.preventDefault();
+    if (event.target === modal) {
+    modal.style.display = "none";
+    };
+  };
+  $(function() {
+    var selectedClass = "";
+    $(".filter").click(function(){
+      selectedClass = $(this).attr("data-rel");
+      $("#gallery").fadeTo(100, 0.1);
+      $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+      setTimeout(function() {
+      $("."+selectedClass).fadeIn().addClass('animation');
+      $("#gallery").fadeTo(300, 1);
+      }, 300);
+    });
+  });
+});
